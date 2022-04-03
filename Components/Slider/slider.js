178aa9e858cf41b53de95723f343617d1bd97fdd
Megha-Joshi <megha24joshi@gmail.com>
2022-04-04 02:43:06 +0530

    proper-spacing

--- Components/Slider/slider.js
+++ Components/Slider/slider.js
@@ -1,9 +1,8 @@
 const sliderInp = document.querySelector(".inp-slider");
 const sliderOut = document.querySelector(".out-slider");
-if(sliderOut && sliderInp)
-{
-sliderOut.textContent = sliderInp.value;
-sliderInp.oninput = function () {
-    sliderOut.textContent = this.value;
-}
+if (sliderOut && sliderInp) {
+    sliderOut.textContent = sliderInp.value;
+    sliderInp.oninput = function () {
+        sliderOut.textContent = this.value;
+    }
 }
